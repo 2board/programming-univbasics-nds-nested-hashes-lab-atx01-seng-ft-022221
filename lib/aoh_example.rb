6754f0b4632f7a75cb9890595489f7c28cb0daae
David Jun <djun301@gmail.com>
2021-02-12 20:56:16 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -31,10 +31,12 @@ def literal_aoh
 end
 
 def aoh_lookup(aoh, row, key)
-  directors[0][:name]
+aoh[row][key]
 end
 
 def aoh_update(aoh, row, key, new_value)
+  aoh[row] = new_value
+  aoh[row]
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
